bbf9fbd0cb0a79d5ed50bbd151166eb3a4998a0f
sonukry <sonuyadav9756@gmail.com>
2020-07-25 16:14:24 +0530

    JSdoc

--- src/Logger/index.js
+++ src/Logger/index.js
@@ -1,33 +1,33 @@
 /* eslint-disable no-console */
 const styles =
   'font-size: 14px;' +
-  'border-radius: 5px; color: #ffffff;'+
-  'padding: 4px 16px 4px 8px; line-height: 30px;';
-
-
+  'border-radius: 2px; color: #ffffff;'+
+  'padding: 3px 5px 3px 5px; line-height: 16px;';
 /**
  * Logs a message and object on console, use this instead of console.log
- * @param message
- * @param data
+ * @param {string} message
+ * @param {any} data
+ * @example log('This is an apple', 'iPhone')
  */
 export function log (message, data) {
   if (data) {
 
     console.log(
-      `%c ${message} `, `${styles} background: #5a6268;`, data
+      `%c ${message} `, `${styles} background: #001628;`, data
     );
   } else {
 
     console.log(
-      `%c ${message} `, `${styles} background: #5a6268;`
+      `%c ${message} `, `${styles} background: #001628;`
     );
   }
 }
 
 /**
  * Logs error on console, use this instead of console.log
- * @param message
- * @param error
+ * @param {string} message
+ * @param {string} error
+ * @example logError('This is an error', '3+2=6')
  */
 export function logError (message, error) {
   // always log errors, even in production env
@@ -44,8 +44,11 @@ export function logError (message, error) {
 
 /**
  * Logs a deprecation message
- * @param message
+ * @param message {string}
  * @param hard {boolean} - should this be a strong warning?
+ * @example
+ * logDeprecation('This method is about to deprecate')
+ * logDeprecation('This method is about to deprecate', true)
  */
 export function logDeprecation (message, hard=false) {
   if (hard) {
