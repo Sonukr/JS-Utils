da372aaff14c1057468a42f7ba87e63a771c90b7
sonukry <sonuyadav9756@gmail.com>
2020-07-28 13:33:11 +0530

    docs

--- src/Logger/index.js
+++ src/Logger/index.js
@@ -1,5 +1,5 @@
 /* eslint-disable no-console */
-const styles =
+const base =
   'font-size: 14px;' +
   'border-radius: 2px; color: #ffffff;'+
   'padding: 3px 5px 3px 5px; line-height: 16px;';
@@ -13,12 +13,35 @@ export function log (message, data) {
   if (data) {
 
     console.log(
-      `%c ${message} `, `${styles} background: #001628;`, data
+      `%c ${message} `, `${base} background: #001628;`, data
     );
   } else {
 
     console.log(
-      `%c ${message} `, `${styles} background: #001628;`
+      `%c ${message} `, `${base} background: #001628;`
+    );
+  }
+}
+
+
+/**
+ * Logs a deprecation message
+ * @param message {string}
+ * @param hard {boolean} - should this be a strong warning?
+ * @example
+ * logDeprecation('This method is about to deprecate')
+ * logDeprecation('This method is about to deprecate', true)
+ */
+export function logDeprecation (message, hard=false) {
+  if (hard) {
+    console.error(
+      '%c --------DEPRECATION WARNING--------',
+      `${base} background: #dc3545;`, `\n${message}`
+    );
+  } else {
+    console.warn(
+      '%c --------DEPRECATION WARNING--------',
+      `${base} background: #ffc107;`, `\n ${message}`
     );
   }
 }
@@ -33,33 +56,11 @@ export function logError (message, error) {
   // always log errors, even in production env
   if (error) {
     console.error(
-      `%c ${message} `, `${styles} background: #dc3545;`, error
+      `%c ${message} `, `${base} background: #dc3545;`, error
     );
   } else {
     console.error(
-      `%c ${message} `, `${styles} background: #dc3545;`
-    );
-  }
-}
-
-/**
- * Logs a deprecation message
- * @param message {string}
- * @param hard {boolean} - should this be a strong warning?
- * @example
- * logDeprecation('This method is about to deprecate')
- * logDeprecation('This method is about to deprecate', true)
- */
-export function logDeprecation (message, hard=false) {
-  if (hard) {
-    console.error(
-      '%c --------DEPRECATION WARNING--------',
-      `${styles} background: #dc3545;`, `\n${message}`
-    );
-  } else {
-    console.warn(
-      '%c --------DEPRECATION WARNING--------',
-      `${styles} background: #ffc107;`, `\n ${message}`
+      `%c ${message} `, `${base} background: #dc3545;`
     );
   }
 }
